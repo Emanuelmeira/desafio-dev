69631ad581d8a9e94de8376d892032623185ff96
Emanuel Meira <emmitmeira@gmail.com>
2021-09-17 17:50:19 -0300

    estrutura inicial de projeto

---
+++ src/test/java/com/bycoders/test/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycoders.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
