c82dd5bebd4c9b3c01069f37f9b144f33dea3b32
Emanuel Meira <emmitmeira@gmail.com>
2021-09-17 22:58:23 -0300

    Service para processamento de arquivo

---
+++ src/main/java/com/bycoders/test/service/ResourceService.java
@@ -0,0 +1,50 @@
+package com.bycoders.test.service;
+
+import com.bycoders.test.domain.Operation;
+import com.bycoders.test.domain.enums.OperationType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+@Service
+public class ResourceService {
+
+    @Autowired
+    private ConverterValueFromFile converterValueFromFile;
+
+    private static final Double VALUE_TO_DOUBT = 100.00;
+
+
+    public void processFile(MultipartFile file) throws IOException {
+
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        var fileLocation = path.substring(0, path.length() - 1) + file.getOriginalFilename();
+
+        Stream<String> lines = Files.lines(Paths.get(fileLocation));
+        lines.forEach(this::processLine);
+    }
+
+    private Operation processLine(String line) {
+
+        var operation = new Operation();
+
+        operation.setOperationType(OperationType.getEnumFromNumType(Integer.parseInt(line.substring(0,1))));
+        operation.setDateOfOperation(converterValueFromFile.converterStringToDate(line.substring(1,9)));
+        operation.setValueOfOperation(Double.parseDouble(line.substring(9,19)) / VALUE_TO_DOUBT);
+        operation.setCpf(line.substring(19,30));
+        operation.setCard(line.substring(30,42));
+        operation.setTimeOfOperation(converterValueFromFile.converterStringToTime(line.substring(42,48)));
+        operation.setStoreOwnerOperation(line.substring(48,62));
+        operation.setStoreNameOperation(line.substring(62,80));
+        return operation;
+
+    }
+
+}
