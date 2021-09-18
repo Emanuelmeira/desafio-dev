ae1d67434c61c0155fac63796423f6189d3f4a05
Emanuel Meira <emmitmeira@gmail.com>
2021-09-18 00:53:52 -0300

    Mapeamento de entidade Operation

--- src/main/java/com/bycoders/test/domain/Operation.java
+++ src/main/java/com/bycoders/test/domain/Operation.java
@@ -2,18 +2,36 @@ package com.bycoders.test.domain;
 
 import com.bycoders.test.domain.enums.OperationType;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
+@Entity
+@Table(name = "OPERATION", schema = "public")
 public class Operation {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "operation_type")
+    @Enumerated(EnumType.STRING)
     private OperationType operationType;
+    @Column(name = "date_operation")
     private LocalDate dateOfOperation;
+    @Column(name = "value_operation")
     private Double valueOfOperation;
+    @Column(name = "cpf")
     private String cpf;
+    @Column(name = "card")
     private String card;
+    @Column(name = "time_operation")
     private LocalTime timeOfOperation;
+    @Column(name = "store_owner_operation")
     private String storeOwnerOperation;
+    @Column(name = "store_name_operation")
     private String storeNameOperation;
 
     public OperationType getOperationType() {
